fd2297a7e476aa9292832270cf9de128afc18d6e
Tioman99 <hluc626@gmail.com>
2023-04-04 13:54:10 +0200

    currency class

--- backend/src/main/java/com/cps_assignment/backend/Currencies.java
+++ backend/src/main/java/com/cps_assignment/backend/Currencies.java
@@ -1,8 +1,34 @@
 package com.cps_assignment.backend;
 
+import java.sql.Timestamp;
+
 public class Currencies {
     private String symbol;
     private String name;
-    private double value;
+    private String flag;
+    private double currencyChange;
+    private double currencyRate;
     private Timestamp time;
+    public String GetSymbol() {
+        return symbol;
+    }
+    public String GetName() {
+        return name;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public double getCurrencyChange() {
+        return currencyChange;
+    }
+
+    public double getCurrencyRate() {
+        return currencyRate;
+    }
+
+    public Timestamp getTime() {
+        return time;
+    }
 }
